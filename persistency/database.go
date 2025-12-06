caf500b1975d6036c02f76f7bcea3c46986f1261
Gustavo <nebesgustavo@gmail.com>
2025-12-05 21:41:48 -0300

    feat: implementa conexão com MySQL e repositório de usuários

---
+++ persistency/database.go
@@ -0,0 +1,25 @@
+package persistency
+
+import (
+	"Api-Aula1/config"
+	"database/sql"
+	"log"
+
+	_ "github.com/go-sql-driver/mysql"
+)
+
+func Connect() (*sql.DB, error) {
+	db, err := sql.Open("mysql", config.Cfg.FormatDSN())
+	if err != nil {
+		log.Println("Erro ao abrir conexão com o DB")
+		return nil, err
+	}
+
+	if err := db.Ping(); err != nil {
+		db.Close()
+		log.Println("Erro ao pingar no DB")
+		return nil, err
+	}
+
+	return db, nil
+}
--- repository/user_repository.go
+++ repository/user_repository.go
@@ -2,41 +2,90 @@ package repository
 
 import (
 	"Api-Aula1/model"
-	"errors"
-	"fmt"
+	"database/sql"
 )
 
-var users []model.User
-var nextID = 1
-
-func Create(user model.User) model.User {
-	user.ID = fmt.Sprintf("%d", nextID)
-	nextID++
-	users = append(users, user)
-	return user
+type UsersRepo struct {
+	db *sql.DB
 }
 
-func FindAll() []model.User {
-	return users
+func NewUsersRepo(db *sql.DB) *UsersRepo {
+	return &UsersRepo{db}
 }
-
-func Update(id string, updatedUser model.User) (model.User, error) {
-	for i, user := range users {
-		if user.ID == id {
-			updatedUser.ID = user.ID
-			users[i] = updatedUser
-			return updatedUser, nil
-		}
+func (repo UsersRepo) Create(user model.User) (uint64, error) {
+	statement, erro := repo.db.Prepare(
+		"INSERT INTO users (name, email, cpf, password) VALUES (?, ?, ?, ?)",
+	)
+	if erro != nil {
+		return 0, erro
 	}
-	return model.User{}, errors.New("utilizador não encontrado")
+	defer statement.Close()
+
+	resultado, erro := statement.Exec(user.Name, user.Email, user.CPF, user.Password)
+	if erro != nil {
+		return 0, erro
+	}
+
+	ultimoIDInserido, erro := resultado.LastInsertId()
+	if erro != nil {
+		return 0, erro
+	}
+
+	return uint64(ultimoIDInserido), nil
 }
 
-func Delete(id string) error {
-	for i, user := range users {
-		if user.ID == id {
-			users = append(users[:i], users[i+1:]...)
-			return nil
-		}
+func (repo UsersRepo) FindAll() ([]model.User, error) {
+	linhas, erro := repo.db.Query("SELECT id, name, email, cpf FROM users")
+	if erro != nil {
+		return nil, erro
 	}
-	return errors.New("utilizador não encontrado")
+	defer linhas.Close()
+
+	var usuarios []model.User
+
+	for linhas.Next() {
+		var usuario model.User
+
+		if erro = linhas.Scan(
+			&usuario.ID,
+			&usuario.Name,
+			&usuario.Email,
+			&usuario.CPF,
+		); erro != nil {
+			return nil, erro
+		}
+
+		usuarios = append(usuarios, usuario)
+	}
+
+	return usuarios, nil
+}
+func (repo UsersRepo) Update(ID uint64, user model.User) error {
+	statement, erro := repo.db.Prepare(
+		"UPDATE users SET name = ?, email = ?, cpf = ? WHERE id = ?",
+	)
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(user.Name, user.Email, user.CPF, ID); erro != nil {
+		return erro
+	}
+
+	return nil
+}
+
+func (repo UsersRepo) Delete(ID uint64) error {
+	statement, erro := repo.db.Prepare("DELETE FROM users WHERE id = ?")
+	if erro != nil {
+		return erro
+	}
+	defer statement.Close()
+
+	if _, erro = statement.Exec(ID); erro != nil {
+		return erro
+	}
+
+	return nil
 }
